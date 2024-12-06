3a8c2099d75e9c2431bb634815e46c4896295549
Favero <74825744+faveroo@users.noreply.github.com>
2024-12-06 11:19:41 -0300

    Create beecrowd2990.sql

---
+++ beecrowd2990.sql
@@ -0,0 +1,6 @@
+SELECT DISTINCT e.cpf, e.enome, d.dnome 
+FROM empregados e
+INNER JOIN departamentos d ON e.dnumero = d.dnumero
+LEFT JOIN trabalha t ON e.cpf = t.cpf_emp
+WHERE t.cpf_emp IS NULL
+ORDER BY e.cpf ASC
